95e9b4182a3eccc83b100de1a79688978a930fdd
zigabait123321 <65978599+zigabait123321@users.noreply.github.com>
2021-02-14 23:38:42 +0300

    create cicle

--- src/main/java/com/tinkoff/Main.java
+++ src/main/java/com/tinkoff/Main.java
@@ -2,55 +2,62 @@ package com.tinkoff;
 
 public class Main {
     public static void main(String[] args) {
-        int[] mass = {8, 6, 3, 1, 7};
+        int[] mass = {8, 6, 3, 1, 7, 9, 11};
         thanosSort(mass);
     }
 
-    public static int[] thanosSort(int[] mass) {
+    public static int[] thanosSort(int[] massToSort) {
+        int sum = 0;
+        int iterationSort;
 
-        int avg = 0; // среднее арфимитическое
-        //boolean isSorted = false; // признак состояния сортировки массива
-        int buffer; // переменная для хранения элемента массива
+        for (iterationSort = 1; iterationSort < massToSort.length; iterationSort++) {
 
-        for (int i = 0; i < mass.length; i++) {
-            avg = avg + mass[i];
-        }
-        System.out.println(avg + " Сумма всех элементов");
-        avg = avg / mass.length;
-        System.out.println(avg + " Среднее арифметическое");
 
-        for (int i = 0; i < mass.length; i++) {
-            System.out.print(mass[i] + " ");
-        }
-        System.out.println("Первый массив");
-
-        int mass2[] = new int[mass.length];
-
-        int elNumHigh = mass2.length; // переменная, сдержащяя крайнее значение элемента, в который
-        int elNumLow = 0;
-        // записываются элементы массива, которые больше сравниваемого знаения
-        System.out.println(elNumHigh + " Количество элементов в mass2");
-
-        for (int i = 0; i < mass.length; i++) {
-
-            System.out.println(mass[i] + " Сравниваемый элемент");
-
-            if (mass[i] <= avg) {
-                mass2[elNumLow] = mass[i];
-                ++elNumLow;
-                System.out.println(mass2[i] + " Элемент масс2 номер " + i);
-            } else {
-                --elNumHigh;
-                mass2[elNumHigh] = mass[i];
+            //Складывем все элементы массива
+            for (int i = 0; i < massToSort.length; i++) {
+                sum += massToSort[i];
             }
+            int avg = sum / massToSort.length;
+            System.out.println(avg + " Среднее арифметическое");
+
+
+
+            //Создаем вспомогательный массив
+            int helpMass[] = new int[massToSort.length/iterationSort];
+            System.out.println(helpMass.length + " Длина нового вспомогательного массива");
+            int max = helpMass.length - 1;
+            int min = 0;
+
+            //Цикл для распределения значений по группам
+            for (int i = 0; i < massToSort.length; i++) {
+                if (massToSort[i] <= avg) {
+                    helpMass[min] = massToSort[i];
+                    ++min;
+                } else {
+                    helpMass[max] = massToSort[i];
+                    --max;
+                }
+            }
+
+            //Цикл для вывода значений второго массива
+            printMass(helpMass);
+            System.out.println(" Второй массив");
+
+            //Присваиваем изначальному массиву отсортированные значения
+            massToSort = helpMass;
+
+            printMass(massToSort);
+            System.out.println(" Первый массив, отсортированный");
+
+            break;
         }
 
-        for (int i = 0; i < mass2.length; i++) {
-            System.out.print(mass2[i] + " ");
+        return massToSort;
+    }
+
+    public static void printMass(int massToPrint[]) {
+        for (int i = 0; i < massToPrint.length; i++) {
+            System.out.print(massToPrint[i] + " ");
         }
-        System.out.println("Второй массив");
-
-
-        return mass;
     }
 }
